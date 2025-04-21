c2d7119174450284d166cdf5e72517381b666ef6
xeizygg <samuelbs99@hotmail.com>
2025-04-21 02:52:54 -0300

    Personagem anda na diagonal

--- src/scenes/Controls.js
+++ src/scenes/Controls.js
@@ -1,39 +1,44 @@
-const defaultVelocity = 250; 
+const defaultVelocity = 250;
+const diagonalVelocity = Math.sqrt((defaultVelocity ** 2) / 2);
 
 export function createControls(scene) {
   return scene.input.keyboard.createCursorKeys();
 }
 
 export function configControls(player, controls, scene) {
-  player.setVelocity(0);
-  player.setVelocityY(0);
-
-  let moving = false;
+  let velocityX = 0;
+  let velocityY = 0;
 
   if (controls.right.isDown) {
+    velocityX = defaultVelocity;
     player.setFlipX(false);
-    player.anims.play("player_walk", true);
-    player.setVelocityX(defaultVelocity);
-    moving = true;
-  } 
-  else if (controls.left.isDown) {
-    player.setFlipX(true);
-    player.anims.play("player_walk", true);
-    player.setVelocityX(-defaultVelocity);
-    moving = true;
-  } 
-  else if (controls.up.isDown) {
-    player.anims.play("walk_down", true);
-    player.setVelocityY(-defaultVelocity);
-    moving = true;
-  } 
-  else if (controls.down.isDown) {
-    player.anims.play("walk_up", true);
-    player.setVelocityY(defaultVelocity);
-    moving = true;
+    player.anims.play("player_walk_right", true);
   }
 
-  if (!moving) {
+  if (controls.left.isDown) {
+    velocityX = -defaultVelocity;
+    player.setFlipX(true);
+    player.anims.play("player_walk_left", true);
+  }
+
+  if (controls.up.isDown) {
+    velocityY = -defaultVelocity;
+    player.anims.play("player_walk_up", true);
+  }
+
+  if (controls.down.isDown) {
+    velocityY = defaultVelocity;
+    player.anims.play("player_walk_down", true);
+  }
+
+  if (velocityX !== 0 && velocityY !== 0) {
+    velocityX = velocityX > 0 ? diagonalVelocity : -diagonalVelocity;
+    velocityY = velocityY > 0 ? diagonalVelocity : -diagonalVelocity;
+  }
+
+  player.setVelocity(velocityX, velocityY);
+
+  if (velocityX === 0 && velocityY === 0) {
     player.anims.play("player_idle", true);
   }
 }
