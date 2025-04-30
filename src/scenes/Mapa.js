47fe6387ed6ab049cc2f6572bc387b4bf163f4de
Iago Molina Camargo <molinakmargo@hotmail.com>
2025-04-30 15:34:49 -0300

    MapFix
    Mapa atualizado

--- src/scenes/Mapa.js
+++ src/scenes/Mapa.js
@@ -1,90 +1,91 @@
-import { configControls, createControls } from "./Controls.js";
-import { createPlayer, loadSprites } from "./Player.js";
-import {
-    loadGoblinSprites,
-    createGoblin
-} from "./Goblin.js";
-import { createEsqueleto, loadEsqueletoSprites } from "./Esqueleto.js";
-import { loadMagoSprites, createMago } from "./Mago.js";
+    import { configControls, createControls } from "./Controls.js";
+    import { createPlayer, loadSprites } from "./Player.js";
+    import {
+        loadGoblinSprites,
+        createGoblin
+    } from "./Goblin.js";
+    import { createEsqueleto, loadEsqueletoSprites } from "./Esqueleto.js";
+    import { loadMagoSprites, createMago } from "./Mago.js";
 
-export class Mapa extends Phaser.Scene {
-    constructor() {
-        super("Mapa");
-        this.player = null;
-        this.goblin = null;
-        this.controls = null;
+    export class Mapa extends Phaser.Scene {
+        constructor() {
+            super("Mapa");
+            this.player = null;
+            this.goblin = null;
+            this.controls = null;
+        }
+
+        preload() {
+            this.load.tilemapTiledJSON("mapa", "assets/mapa_vila_floresta.json");
+            this.load.image("tileset_cave", "assets/map/constructions/cave.png");
+            this.load.image("tileset_grass", "assets/map/blocos/tileset_grass.png");
+            this.load.image("tileset_water", "assets/map/blocos/tileset_water.png");
+            this.load.image("tiled_route", "assets/map/constructions/tiled_route.png");
+            this.load.image("tiled_extras", "assets/map/constructions/tiled_extras_noBG.png");
+            this.load.image("tileset_houses", "assets/map/constructions/tileset_houses_noBG.png");
+            this.load.image("tileset_houses2", "assets/map/constructions/tileset_houses2_noBG.png");
+            this.load.image("tileset_abandoned_houses", "assets/map/constructions/tileset_abandoned_houses.png");
+            this.load.image("tileset_three", "assets/map/constructions/tileset_three.png");
+
+            loadSprites(this);
+            loadGoblinSprites(this);
+            loadEsqueletoSprites(this);
+            loadMagoSprites(this)
+        }
+
+        create() {
+            const map = this.make.tilemap({ key: "mapa" });
+
+            const tilesets = [
+                map.addTilesetImage("tileset_cave", "tileset_cave"),
+                map.addTilesetImage("tileset_grass", "tileset_grass"),
+                map.addTilesetImage("tileset_water", "tileset_water"),
+                map.addTilesetImage("tiled_route", "tiled_route"),
+                map.addTilesetImage("tiled_extras", "tiled_extras"),
+                map.addTilesetImage("tileset_houses", "tileset_houses"),
+                map.addTilesetImage("tileset_houses2", "tileset_houses2"),
+                map.addTilesetImage("tileset_abandoned_houses", "tileset_abandoned_houses"),
+                map.addTilesetImage("tileset_three", "tileset_three"),
+            ];
+
+            const layer1 = map.createLayer("Camada de Blocos 1", tilesets, 0, 0);
+            const layer2 = map.createLayer("Camada de Blocos 2", tilesets, 0, 0);
+
+            layer1.setCollisionByProperty({ collider: true });
+            layer2.setCollisionByProperty({ collider: true });
+
+            this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+            this.player = createPlayer(this);
+            this.player.setPosition(64, map.heightInPixels - 64);
+            this.player.setCollideWorldBounds(true);
+
+            this.cameras.main.startFollow(this.player);
+            this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+            this.controls = createControls(this);
+
+            this.physics.add.collider(this.player, layer1);
+            this.physics.add.collider(this.player, layer2);
+
+            // goblin
+            this.goblin = createGoblin(this);
+            this.goblin.setPosition(this.player.x + 64, this.player.y);
+
+            // esqueleto
+            this.esqueleto = createEsqueleto(this);
+            this.esqueleto.setPosition(this.player.x + 100, this.player.y);
+
+            // mago
+            this.mago = createMago(this);
+            this.mago.setPosition(this.player.x + 150, this.player.y);
+
+            this.physics.add.collider(this.player, this.goblin, () => {
+                console.log("Colidiu com o goblin!");
+            });
+        }
+
+        update() {
+            configControls(this.player, this.controls);
+        }
     }
-
-    preload() {
-        this.load.tilemapTiledJSON("mapa", "assets/mapa_vila_floresta.json");
-
-        this.load.image("tileset_grass", "assets/map/blocos/tileset_grass.png");
-        this.load.image("tileset_water", "assets/map/blocos/tileset_water.png");
-        this.load.image("tiled_route", "assets/map/constructions/tiled_route.png");
-        this.load.image("tiled_extras", "assets/map/constructions/tiled_extras_noBG.png");
-        this.load.image("tileset_houses", "assets/map/constructions/tileset_houses_noBG.png");
-        this.load.image("tileset_houses2", "assets/map/constructions/tileset_houses2_noBG.png");
-        this.load.image("tileset_abandoned_houses", "assets/map/constructions/tileset_abandoned_houses.png");
-        this.load.image("tileset_three", "assets/map/constructions/tileset_three.png");
-
-        loadSprites(this);
-        loadGoblinSprites(this);
-        loadEsqueletoSprites(this);
-        loadMagoSprites(this)
-    }
-
-    create() {
-        const map = this.make.tilemap({ key: "mapa" });
-
-        const tilesets = [
-            map.addTilesetImage("tileset_grass", "tileset_grass"),
-            map.addTilesetImage("tileset_water", "tileset_water"),
-            map.addTilesetImage("tiled_route", "tiled_route"),
-            map.addTilesetImage("tiled_extras", "tiled_extras"),
-            map.addTilesetImage("tileset_houses", "tileset_houses"),
-            map.addTilesetImage("tileset_houses2", "tileset_houses2"),
-            map.addTilesetImage("tileset_abandoned_houses", "tileset_abandoned_houses"),
-            map.addTilesetImage("tileset_three", "tileset_three"),
-        ];
-
-        const layer1 = map.createLayer("Camada de Blocos 1", tilesets, 0, 0);
-        const layer2 = map.createLayer("Camada de Blocos 2", tilesets, 0, 0);
-
-        layer1.setCollisionByProperty({ collider: true });
-        layer2.setCollisionByProperty({ collider: true });
-
-        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-        this.player = createPlayer(this);
-        this.player.setPosition(64, map.heightInPixels - 64);
-        this.player.setCollideWorldBounds(true);
-
-        this.cameras.main.startFollow(this.player);
-        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-        this.controls = createControls(this);
-
-        this.physics.add.collider(this.player, layer1);
-        this.physics.add.collider(this.player, layer2);
-
-        // goblin
-        this.goblin = createGoblin(this);
-        this.goblin.setPosition(this.player.x + 64, this.player.y);
-
-        // esqueleto
-        this.esqueleto = createEsqueleto(this);
-        this.esqueleto.setPosition(this.player.x + 100, this.player.y);
-
-        // mago
-        this.mago = createMago(this);
-        this.mago.setPosition(this.player.x + 150, this.player.y);
-
-        this.physics.add.collider(this.player, this.goblin, () => {
-            console.log("Colidiu com o goblin!");
-        });
-    }
-
-    update() {
-        configControls(this.player, this.controls);
-    }
-}
